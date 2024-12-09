ab996d72187ea55ffdc44b2f19c43add6ac93fa2
CramY2001 <szymek.kramarczyk@gmail.com>
2024-12-10 00:58:57 +0100

    Update foo.hpp

--- foo.hpp
+++ foo.hpp
@@ -1,16 +1,18 @@
-#pragma once
-
 #include "Human.hpp"
 
 #include <algorithm>
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
-{
-    std::vector< char > retval(people.size());
-
-    // Twoja implementacja tutaj
+std::vector<char> foo(std::list<Human>& people) {
+    std::for_each(people.begin(), people.end(), [](Human& person) {
+        person.birthday();
+    });
+    std::vector<char> retval;
+    retval.reserve(people.size());
+    std::transform(people.rbegin(), people.rend(), std::back_inserter(retval), [](const Human& person) {
+        return person.isMonster() ? 'n' : 'y';
+    });
 
     return retval;
 }
